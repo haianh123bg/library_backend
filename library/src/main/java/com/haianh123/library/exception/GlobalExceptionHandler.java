339331b26e4c01075e4c68f5a5684cbaf679a7dd
haianh123bg <nguyenngochaianh20033@gmail.com>
2024-04-26 14:53:49 +0700

    change global exception

--- library/src/main/java/com/haianh123/library/exception/GlobalExceptionHandler.java
+++ library/src/main/java/com/haianh123/library/exception/GlobalExceptionHandler.java
@@ -1,9 +1,87 @@
 package com.haianh123.library.exception;
 
+import com.haianh123.library.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse> handlingAppException(AppException exception){
+        ErrorCode errorCode = exception.getErrorCode();
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(apiResponse);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException exception){
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(ErrorCode.UNCATEGORIED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIED_EXCEPTION.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = IllegalStateException.class)
+    ResponseEntity<ApiResponse> handlingIllegalStateException(IllegalStateException exception){
+        ApiResponse apiResponse = new ApiResponse();
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<Map<String, String>> handlingMethodArgumentNotValidException(MethodArgumentNotValidException exception){
+        String enumKey = exception.getFieldError().getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, String> attributes = new HashMap<>();
+        errorCode = ErrorCode.valueOf(enumKey);
+
+        exception.getBindingResult()
+                .getAllErrors().forEach((error) ->{
+                    String fieldName = ((FieldError)error).getField();
+                    String message = error.getDefaultMessage();
+                    attributes.put(fieldName, message);
+                });
+
+        return ResponseEntity.badRequest().body(attributes);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
