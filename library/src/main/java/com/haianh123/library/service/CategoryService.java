f649105306fa797bd64b257ef1be1387f476d90c
NgoVanLoi123 <126578909+NgoVanLoi123@users.noreply.github.com>
2024-04-26 11:06:21 +0700

    Add CategoryService

---
+++ library/src/main/java/com/haianh123/library/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.haianh123.library.service;
+
+import com.haianh123.library.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryService{
+}
